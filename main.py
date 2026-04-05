5a5c0677b3a08d156c3adbbbca396a80c46f7568
Tabish <tabishmusheer17@gmail.com>
2026-04-06 03:27:37 +0500

    of

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def capture_and_score_lead(lead: IncomingLead, api_key: str = Depends(get_
         logger.info(f"✅ AI Analysis Complete. Category: {ai_analysis.category}, Score: {ai_analysis.lead_score}")
         
         # Make.com Webhook URL
-        MAKE_WEBHOOK_URL = "https://hook.us1.make.com/5bjeyexi26nbyhxf9e70gn5dpxrrldah"
+        MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/5b3eqexi26nbyhxf9e70gn5dpxrrldah"
         
         # Send Data to Make.com
         logger.info("🚀 Data Make.com par bhej rahe hain...")
